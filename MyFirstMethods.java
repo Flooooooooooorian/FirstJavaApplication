01e09f6dc06d2ffb575b77b7589731dca218b5a8
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2023-02-28 17:17:21 +0100

    Add Methods

---
+++ MyFirstMethods.java
@@ -0,0 +1,40 @@
+public class MyFirstMethods {
+
+    public static void main(String[] args) {
+        int resultSum = sum(16, 25);
+        String resultString = sumAsString(68, 4);
+
+        System.out.println(resultSum);
+        System.out.println(resultString);
+
+
+        String name = "Florian";
+
+        System.out.println(name.toLowerCase());
+
+        System.out.println(isLongerThan20("NotLongEnough"));
+        System.out.println(containsFancy("YeahFancy"));
+
+        System.out.println(longerThan20AndContainsFancy("ThisStringContainsTheWordfancy"));
+    }
+
+    public static int sum(int valueA, int valueB) {
+        return valueA + valueB;
+    }
+
+    public static String sumAsString(int valueA, int valueB) {
+        return "Summe: " + (valueA + valueB);
+    }
+
+    public static boolean isLongerThan20(String text) {
+        return text.length() > 20;
+    }
+
+    public static boolean containsFancy(String text) {
+        return text.contains("fancy");
+    }
+
+    public static boolean longerThan20AndContainsFancy(String text) {
+        return isLongerThan20(text) && containsFancy(text);
+    }
+}
---
+++ out/production/FirstJavaApplication/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+K
+MyFirstJavaApplication.java,d/1/d1871ec5828a4f775468b96d995a5989e398dc0e
